1a93030ea4b4a818d89d9cbcf00ac793b3f3e691
aguadoeduardo <e.aguado.2021@alumnos.urjc.es>
2023-11-13 15:53:30 +0100

    Comprobacion con API

--- renderer.js
+++ renderer.js
@@ -51,3 +51,45 @@ const func = async () => {
   }
   
   func()
+
+
+const form = document.querySelector('form');
+const comprobacionBtn = document.getElementById('comprobacion-btn');
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const cadenaInput = document.getElementById('cadena');
+  const cadena = cadenaInput.value;
+
+  // Realizar la solicitud POST a la API
+  try {
+    const response = await fetch('http://localhost:8080/api/comprobacion', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ cadena })
+    });
+
+    if (response.ok) {
+      const resultado = await response.json();
+      if (resultado) {
+        showToast('liveToastSeguro'); // Mostrar mensaje de contraseña segura
+      } else {
+        showToast('liveToastNoSeguro'); // Mostrar mensaje de contraseña no segura
+      }
+    } else {
+      console.error('Error en la solicitud');
+    }
+  } catch (error) {
+    console.error('Error en la solicitud:', error);
+  }
+});
+
+// Función para mostrar los mensajes Toast
+function showToast(id) {
+  const toast = document.getElementById(id);
+  const bsToast = new bootstrap.Toast(toast);
+  bsToast.show();
+}
