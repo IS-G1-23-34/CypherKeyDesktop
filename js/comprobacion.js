33385fff5a7d73e0e64171ba2c3c3ae468ff6006
aalmeidar <aalmeidar@protonmail.com>
2023-12-16 18:24:53 +0100

    ISG12324-82 Partes de la contraseña que no se cumplen

--- js/comprobacion.js
+++ js/comprobacion.js
@@ -36,3 +36,55 @@ function showToast(id) {
     const bsToast = new bootstrap.Toast(toast);
     bsToast.show();
 }
+
+document.getElementById("cadena")
+    .addEventListener("keyup", function(event) {
+        event.preventDefault();
+        if (event.keyCode === 13) {
+            document.getElementById("comprobacion-btn").click();
+        }
+        check()
+    });
+
+function check() {
+    const cadena = document.getElementById("cadena").value
+    if (cadena !== "") {
+        const newDiv = document.createElement("div")
+        let secure = true
+        newDiv.classList.add('alert')
+        newDiv.classList.add('alert-danger')
+        newDiv.classList.add('mt-2')
+        newDiv.role = 'alert'
+        newDiv.id = 'alerta'
+        if (cadena.length < 8) {
+            newDiv.appendChild(document.createTextNode("La longitud es menor que 8"))
+            newDiv.appendChild(document.createElement('br'))
+            secure = false
+        }
+        if (!/[A-Z]+/.test(cadena)) {
+            newDiv.appendChild(document.createTextNode("No contiene mayusculas"))
+            newDiv.appendChild(document.createElement('br'))
+            secure = false
+        }
+        if (!/[a-z]+/.test(cadena)) {
+            newDiv.appendChild(document.createTextNode("No contiene minusculas"))
+            newDiv.appendChild(document.createElement('br'))
+            secure = false
+        }
+        if (!/[0-9]+/.test(cadena)) {
+            newDiv.appendChild(document.createTextNode("No contiene numeros"))
+            newDiv.appendChild(document.createElement('br'))
+            secure = false
+        }
+        if (!/[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(cadena)) {
+            newDiv.appendChild(document.createTextNode("No contiene caracteres especiales"))
+            secure = false
+        }
+        if (document.getElementById("alerta") != null) {
+            document.getElementById("alerta").remove()
+        }
+        if (!secure) {
+            document.getElementById("submit-btn").appendChild(newDiv)
+        }
+    }
+}
